98cd0c78baa0840f0e9a64d446d7357c4d86be0a
likevishal <ivishalonline@gmail.com>
2026-04-10 14:50:25 +0530

    Add agent dashboard

---
+++ resources/views/agent/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.agent')
+
+@section('content')
+
+<h2>Dashboard</h2>
+
+<div class="row mt-4">
+
+    <div class="col-md-4">
+        <div class="card p-3">
+            <h5>Total Customers</h5>           
+        </div>
+    </div>
+
+</div>
+
+@endsection
