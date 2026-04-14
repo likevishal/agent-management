1d7595a0a1221ffb389bdd63578ec91d540a9176
likevishal <ivishalonline@gmail.com>
2026-04-14 14:03:32 +0530

    Adding a server function for show reset form and reset password

--- app/Http/Controllers/AgentForgotPasswordController.php
+++ app/Http/Controllers/AgentForgotPasswordController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\PasswordResetToken;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\AgentResetPasswordMail;
+use App\Models\Agent;
+use Illuminate\Support\Facades\Hash;
 
 class AgentForgotPasswordController extends Controller
 {
@@ -38,5 +40,38 @@ class AgentForgotPasswordController extends Controller
         Mail::to($request->email)->send(new AgentResetPasswordMail($token));
 
         return back()->with('success', 'Reset link successfully');
+    }
+
+    public function showResetForm($token)
+    {
+        return view('agent.auth.reset', compact('token'));
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ]);
+
+        $record = PasswordResetToken::where([
+            'email' => $request->email,
+            'token' => $request->token
+        ])->first();
+
+        if (!$record) {
+            return back()->with('error', 'Invalid Request');
+        }
+
+        if (now()->diffInMinutes($record->created_at) > 60) {
+            return back()->with('error', 'token expired');
+        }
+
+        Agent::where('email', $request->email)
+            ->update([
+                'password' => Hash::make($request->password)
+            ]);
+
+        return redirect()->route('agent.login')->with('success', 'Password reset successful.');
     }
 }
