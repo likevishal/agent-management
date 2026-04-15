8d715eb872912ba69ef0e6fb35db99aa5891d72f
likevishal <ivishalonline@gmail.com>
2026-04-15 14:09:52 +0530

    Adding page for customers

---
+++ frontend/app/customers/page.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Customers() {
+    return (
+        <div style={{padding : 20}}>
+            <h2>Customers page</h2>
+            <p>We will load Customers here..</p>
+        </div>
+    )
+}
